13912df7b14403fe1840c48b6f02a1cceda2ae77
SuperNinja <superninja@ninjatech.ai>
2026-05-29 05:02:31 +0000

    fix: wrap app with language provider for BMI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,16 @@ import { Router, Route } from "wouter";
 import Home from "./pages/Home";
 import CategoryPage from "./pages/CategoryPage";
 import ToolPage from "./pages/ToolPage";
+import { LanguageProvider } from "./contexts/LanguageContext";
 
 export default function App() {
   return (
-    <Router>
-      <Route path="/" component={Home} />
-      <Route path="/category/:category" component={CategoryPage} />
-      <Route path="/tools/:category/:toolName" component={ToolPage} />
-    </Router>
+    <LanguageProvider>
+      <Router>
+        <Route path="/" component={Home} />
+        <Route path="/category/:category" component={CategoryPage} />
+        <Route path="/tools/:category/:toolName" component={ToolPage} />
+      </Router>
+    </LanguageProvider>
   );
 }
